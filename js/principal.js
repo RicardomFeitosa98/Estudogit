56a1247975e41a6fb3d1495f13d0163d54939762
Ricardo <ricardomfeitosa98@gmail.com>
2023-01-27 18:15:11 -0300

    ajeitando para rebase"

--- js/principal.js
+++ js/principal.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 var banners =
 ["Os melhores do Brasil!", "Qualidade e preço baixo!"];
 var bannerAtual = 0;
@@ -6,4 +7,15 @@ bannerAtual = (bannerAtual + 1) % 2;
 document.querySelector('h2#mensagem').textContent =
 banners[bannerAtual];
 }
+=======
+var banners =
+["Os melhores do Brasil!", "Qualidade e preço baixo!"];
+var banners = ["Do lixo ao luxo!","Reaproveitar é aproveitar!"];
+var bannerAtual = 0;
+function trocaBanner() {
+bannerAtual = (bannerAtual + 1) % 2;
+document.querySelector('h2#mensagem').textContent =
+banners[bannerAtual];
+}
+>>>>>>> 88b8bce (alterando textos do banner)
 setInterval(trocaBanner, 1000);
